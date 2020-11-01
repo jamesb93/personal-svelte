35b5c454f895e813f73b04bee363ca85d31fa9c5
James Bradbury <jamesbradbury93@gmail.com>
2020-11-01 09:50:41 +0000

    collapsible nav

--- src/components/StaticNav.svelte
+++ src/components/StaticNav.svelte
@@ -1,33 +1,35 @@
 <script>
 	export let segment;
+	import { fade } from 'svelte/transition';
 </script>
 
 <style>
-	ul {
-		font-size: 12px;
-		margin-top: 7em;
+	nav {
 		min-width: max-content;
 		max-width: max-content;
+		padding-left: 0;
+		transition: 0.1s;
+		height: 100%;
+		padding-right: 1em;
+		z-index: 200;
+	}
+
+	ul {
+		font-size: 12px;
 		color: rgb(127, 127, 127);
 		word-wrap: none;
 		padding-left: 0;
-		position: fixed;
-		margin-left: 5em;
+		opacity: 1;
+		padding-top: 5em;
 	}
 
 	ul li {
-		flex: 1;
 		display: block;
 	}
 
 	.selected {
-		position: relative;
 		display: inline-block;
 		color: rgb(0, 0, 0)
-	}
-
-	.project {
-		margin-left: 1em;
 	}
 
 	
@@ -48,35 +50,43 @@
 	a:hover {
 		color: rgb(178, 178, 178)
 	}
-	
-    @media only screen and (max-width: 1296px) {
-		ul {display: none}
-	}
 
 	.delim {
 		color:black;
 	}
+
+	.container {
+		position: fixed;
+		height: 100%;
+		background-color: white;
+		z-index: 99;
+	}
 </style>
-<ul>
-	<li><a rel=prefetch class:selected='{segment === undefined}'  		href='.'>James Bradbury</a></li>
-	<i>james.bradbury@hud.ac.uk</i>
-	<br><br>
-	<span class="delim">info</span>
-	<li><a rel=prefetch class:selected='{segment === "music"}'  		href='music'>music</a></li>
-	<li><a rel=prefetch class:selected='{segment === "media"}'  		href='media'>media</a></li>
-	<li><a rel=prefetch class:selected='{segment === "performances"}'   href='performances'>performances</a></li>
-	<li><a rel=prefetch class:selected='{segment === "code"}'  			href='code'>code</a></li>
-	<li><a rel=prefetch class:selected='{segment === "publications"}'   href='publications'>publications</a></li>
-	<br>
-	<span class="delim">projects</span>
-	<li><a rel=prefetch class:selected='{segment === "reacoma"}'   				href='reacoma'>ReaCoMa</a></li>
-	<li><a rel=prefetch class:selected='{segment === "annealing-strategies"}'   href='annealing-strategies'>annealing strategies</a></li>
-	<li><a rel=prefetch class:selected='{segment === "biomimicry"}'   			href='biomimicry'>biomimicry</a></li>
-	<br>
-	<span class="delim">links</span>
-	<li><a target="blank" href="https://www.github.com/jamesb93">Github</a></li>
-	<li><a rel=prefetch class:selected='{segment === "tech"}'   			href='tech'>tech/honourable mentions</a></li>
+
+<div transition:fade class="container" id="nav-container">
+	<nav transition:fade id="nav">
+		<ul transition:fade>
+			<br>
+			<li><a rel=prefetch class:selected='{segment === undefined}'  		href='.'>James Bradbury</a></li>
+			<i>james.bradbury@hud.ac.uk</i>
 	
+			<br><br>
+			<span class="delim">info</span>
+			<li><a rel=prefetch class:selected='{segment === "music"}'  		href='music'>music</a></li>
+			<li><a rel=prefetch class:selected='{segment === "media"}'  		href='media'>media</a></li>
+			<li><a rel=prefetch class:selected='{segment === "performances"}'   href='performances'>performances</a></li>
+			<li><a rel=prefetch class:selected='{segment === "code"}'  			href='code'>code</a></li>
+			<li><a rel=prefetch class:selected='{segment === "publications"}'   href='publications'>publications</a></li>
+			<br>
+			<span class="delim">projects</span>
+			<li><a rel=prefetch class:selected='{segment === "reacoma"}'   				href='reacoma'>ReaCoMa</a></li>
+			<li><a rel=prefetch class:selected='{segment === "annealing-strategies"}'   href='annealing-strategies'>annealing strategies</a></li>
+			<li><a rel=prefetch class:selected='{segment === "biomimicry"}'   			href='biomimicry'>biomimicry</a></li>
+			<br>
+			<span class="delim">links</span>
+			<li><a target="blank" href="https://www.github.com/jamesb93">Github</a></li>
+			<li><a rel=prefetch class:selected='{segment === "tech"}'   			href='tech'>tech/honourable mentions</a></li>
+		</ul>
+	</nav>
+</div>
 
-
-</ul>
