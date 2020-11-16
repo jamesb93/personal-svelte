14ebf405b5875a6586277e165ee3c14490cdbb81
James Bradbury <jamesbradbury93@gmail.com>
2020-11-16 00:04:53 +0000

    components for rending media easily

---
+++ src/components/ImageMedia.svelte
@@ -0,0 +1,30 @@
+<style>
+
+    .container :global(img) {
+        max-width: 100%;
+    }
+
+    .caption :global(p) {
+        font-style: italic;
+        text-align: center;
+    }
+
+
+
+    span {
+        color: red;
+        font-weight: 700;
+    }
+</style>
+
+<div class="container">
+    <slot name="media">
+        <span>There should be media content here. If there isn't there is a problem</span>
+    </slot>
+    <div class="caption">
+        <slot name="caption">
+            <span>There should be a caption or attribution here</span>
+        </slot>
+    </div>
+</div>
+
---
+++ src/components/VideoMedia.svelte
@@ -0,0 +1,17 @@
+<style>
+    .container {text-align: center}
+
+    p {
+        color: red;
+        font-weight: 700;
+    }
+</style>
+
+<div class="container">
+    <slot name="media">
+        <p>
+            There should be media content here. If there isn't there is a problem.
+        </p>
+    </slot>
+</div>
+
